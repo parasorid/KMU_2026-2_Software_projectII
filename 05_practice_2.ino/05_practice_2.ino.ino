254c7aee6f5dd017b03fb775cfcd8fd4cbed103d
parasorid <parasorid@naver.com>
2026-09-16 10:11:23 +0900

    실습 3

---
+++ 05_practice_2.ino/05_practice_2.ino.ino
@@ -0,0 +1,24 @@
+#define LED_PIN 7
+void setup() {
+  pinMode(7, OUTPUT);
+  digitalWrite(7, 1);
+}
+
+void loop() {
+  int cnt = 0;
+  digitalWrite(LED_PIN, 0);
+  delay(1000);
+  digitalWrite(LED_PIN, 1);
+  while(true){
+    digitalWrite(LED_PIN, 0);
+    delay(100);
+    digitalWrite(LED_PIN, 1);
+    delay(100);    
+    cnt+=1;
+    if(cnt==5)
+      break;
+  }
+  while(true){
+    ;
+  }
+}
